adaf07a2ad7cfce5186e8f6b14e40b197fb20244
Jasmeet Singh <jasmeet0915@gmail.com>
2020-09-18 02:26:21 +0530

    Utils.kt helper methods added for JSON parsing

--- remedE-app/app/src/main/java/com/example/remede/Utils.kt
+++ remedE-app/app/src/main/java/com/example/remede/Utils.kt
@@ -3,13 +3,30 @@ package com.example.remede
 import android.content.Context
 import android.os.Build
 import androidx.annotation.RequiresApi
-//import com.example.remede.model.MedData
-//import com.google.gson.Gson
-//import com.google.gson.JsonParser
-//import com.google.gson.reflect.TypeToken
+import com.example.remede.model.MedData
+import com.google.gson.Gson
+import com.google.gson.JsonParser
+import com.google.gson.reflect.TypeToken
 import java.time.LocalTime
 
 object Utils {
+    private fun getJsonFromAsset(context: Context, fileName: String): String{
+        return context.assets.open(fileName).bufferedReader().use { it.readText() }
+    }
+
+    fun getMedNames(context: Context): Set<String>{
+        val json = getJsonFromAsset(context, "med_data.json")
+        return JsonParser().parse(json).asJsonObject.keySet()
+    }
+
+    fun getMedDetails(context: Context, medName: String?): MedData {
+        val json = getJsonFromAsset(context, "med_data.json")
+        val medJson = JsonParser().parse(json).asJsonObject.get(medName)
+        val gson = Gson()
+        val medDataType = object : TypeToken<MedData>() {}.type
+        return gson.fromJson(medJson, medDataType)
+    }
+
     @RequiresApi(Build.VERSION_CODES.O)
     fun getNextDose(times: Map<String, Boolean>): String {
         val currTime = LocalTime.now()
