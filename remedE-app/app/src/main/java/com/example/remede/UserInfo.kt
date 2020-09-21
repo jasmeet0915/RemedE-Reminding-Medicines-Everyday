84ee0183f7a60c6468101f9d897c6615c33b31a7
prabhsimar100 <prabhsimar100@gmail.com>
2020-09-21 06:06:24 +0530

    Utils file added

---
+++ remedE-app/app/src/main/java/com/example/remede/UserInfo.kt
@@ -0,0 +1,12 @@
+package com.example.remede
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UserInfo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_info)
+    }
+
+}
