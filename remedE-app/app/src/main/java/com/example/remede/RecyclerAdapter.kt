86372b0d26e459961d011bdc9db626a4c3a1173d
prabhsimar100 <prabhsimar100@gmail.com>
2020-09-25 18:13:33 +0530

    toggle on click listener added to Recycler Adapter

--- remedE-app/app/src/main/java/com/example/remede/RecyclerAdapter.kt
+++ remedE-app/app/src/main/java/com/example/remede/RecyclerAdapter.kt
@@ -1,9 +1,11 @@
 package com.example.remede
 import android.content.Intent
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.Switch
 import android.widget.TextView
 import androidx.appcompat.view.menu.ActionMenuItemView
 import androidx.recyclerview.widget.RecyclerView
@@ -18,6 +20,7 @@ class RecyclerAdapter(private val medNames: List<String>,
         val itemName: TextView = itemView.findViewById(R.id.medName)
         val itemNextDose: TextView = itemView.findViewById(R.id.nextDose)
         val pillImage: ImageView = itemView.findViewById(R.id.pillIcon)
+        val takenSwitch: Switch = itemView.findViewById(R.id.takenSwitch)
 
         init {
             itemView.setOnClickListener{v: View ->
@@ -29,6 +32,16 @@ class RecyclerAdapter(private val medNames: List<String>,
                 v.context.startActivity(intent)
             }
         }
+        init{
+            takenSwitch.setOnCheckedChangeListener{ _ , isChecked ->
+                val message = isChecked
+                if (isChecked){
+                    Log.i("RecyclerAdapter", "medicine taken")
+                }
+            }
+
+        }
+
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
